8ff76b9635fe67ee1b9141398d393027d76a08b3
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-06-05 00:07:12 +0900

    unstake delay changed to 15 mins

--- contracts/eosio.system/delegate_bandwidth.cpp
+++ contracts/eosio.system/delegate_bandwidth.cpp
@@ -28,7 +28,7 @@ namespace eosiosystem {
    using std::map;
    using std::pair;
 
-   static constexpr time refund_delay = 3*24*3600;
+   static constexpr time refund_delay = 60*15; // change unstake refund deplay to 15 mins for test
    static constexpr time refund_expiration_time = 3600;
 
    struct user_resources {
